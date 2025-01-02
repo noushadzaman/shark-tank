e285d2ef1fc157a3cf8bc57cd3d40a44194142df
Noushad <noushadozi333@gmail.com>
2025-01-02 18:57:00 +0600

    auth with github

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="font-work-sans">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
