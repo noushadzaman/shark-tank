0ceb462cfb5d8fde22a5b9505947bb1517ba9280
Noushad <noushadozi333@gmail.com>
2025-01-03 17:02:50 +0600

    searchbar home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import SearchForm from "../../components/home/SearchForm";
+
+// export default function Home({searchParams}: {searchParams: Promise<{ query?: string }>}) {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Launch your project to a new era</h1>
+        <p className="sub-heading !max-w-3xl">
+          Present your project, vote on projects and get noticed in virtual
+          realm.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
